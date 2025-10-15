07a24d763485f5930da8ed3289b666e2cb8a5750
eggrusher04 <rushaidy@live.com>
2025-10-15 14:20:42 +0800

    Tested Search API
    Successfully search eateries by address, building name, postalcode. Able to pull every eateries as well

--- Lab3/HealthyAuraV2/HealthyAura/src/main/java/com/FeedEmGreens/HealthyAura/controller/EateryController.java
+++ Lab3/HealthyAuraV2/HealthyAura/src/main/java/com/FeedEmGreens/HealthyAura/controller/EateryController.java
@@ -5,27 +5,27 @@ import com.FeedEmGreens.HealthyAura.dto.EateryRequest;
 import com.FeedEmGreens.HealthyAura.service.EateryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/eateries")
 public class EateryController {
     @Autowired
     private EateryService eateryService;
 
     @GetMapping
-    public ResponseEntity<List<EateryRequest>> getAllEateries(){
-        return ResponseEntity.ok(eateryService.fetchEateries());
-    }
-
-    @GetMapping
-    public ResponseEntity<List<EateryRequest>> search(@RequestParam(required = false) String query){
-        List<EateryRequest> results = eateryService.searchEatery(query);
-        return ResponseEntity.ok(results);
+    public ResponseEntity<List<EateryRequest>> getAllEateries(@RequestParam(required = false) String query){
+        if(query == null || query.isBlank()) {
+            return ResponseEntity.ok(eateryService.fetchEateries());
+        }
+        else{
+            List<EateryRequest> results = eateryService.searchEatery(query);
+            return ResponseEntity.ok(results);
+        }
     }
 }
