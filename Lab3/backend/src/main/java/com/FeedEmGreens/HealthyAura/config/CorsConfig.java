032a1b327c866271aa021ce9260166038e311e5e
eggrusher04 <rushaidy@live.com>
2025-11-08 00:44:16 +0800

    merged gaining point features

--- Lab3/backend/src/main/java/com/FeedEmGreens/HealthyAura/config/CorsConfig.java
+++ Lab3/backend/src/main/java/com/FeedEmGreens/HealthyAura/config/CorsConfig.java
@@ -4,9 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer{
-
 
     public void addCorsMapping(CorsRegistry registry){
         registry.addMapping("/**")
